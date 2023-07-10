c29fda5c9437f1651647fe62abff174d63c8ec41
Kevin Glass <kglass@cafex.com>
2023-07-10 21:13:28 +0100

    Remove test spawn position

--- src/mods/defaultmods/PickaxeMod.ts
+++ src/mods/defaultmods/PickaxeMod.ts
@@ -11,9 +11,6 @@ export class PickaxeMod implements ServerMod {
     pickAxeType: string = "";
 
     onGameStart(game: GameContext): void {
-        game.setGameProperty(GameProperty.SPAWN_X, 50);
-        game.setGameProperty(GameProperty.SPAWN_Y, 2);
-
         this.pickAxeType = game.addTool("holding/pick_iron", 0, "iron-pick", false, true, 60, true, 0.01);
     }
     
